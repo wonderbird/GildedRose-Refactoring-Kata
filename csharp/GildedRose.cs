d91241702ae28e562293df992e11aa9441a4fcf0
Stefan Boos <kontakt@boos.systems>
2020-02-03 14:37:40 +0100

    refactoring if statements

--- csharp/GildedRose.cs
+++ csharp/GildedRose.cs
@@ -8,34 +8,26 @@ namespace csharp
         public const string BackstagePasses = "Backstage passes to a TAFKAL80ETC concert";
         public const string Sulfuras = "Sulfuras, Hand of Ragnaros";
     }
-    
+
     public class GildedRose
     {
         public const int MaxQuality = 50;
         public const int BackstagePassesThresholdDoubleQuality = 11;
-        private const int BackstagePassesThresholdTripleQuality = 6;
+        public const int BackstagePassesThresholdTripleQuality = 6;
         public const int MinQuality = 0;
         public const int QualityDecreaseStep = 1;
         public const int QualityIncreaseStep = 1;
-        readonly IList<Item> Items;
-        public static int SellInDecreaseStep;
+        public const int SellInDecreaseStep = 1;
+        private readonly IList<Item> Items;
 
         public GildedRose(IList<Item> Items)
         {
             this.Items = Items;
         }
 
-        static GildedRose()
-        {
-            SellInDecreaseStep = 1;
-        }
-
         public void UpdateQuality()
         {
-            foreach (var item in Items)
-            {
-                UpdateItemQuality(item);
-            }
+            foreach (var item in Items) UpdateItemQuality(item);
         }
 
         private static void UpdateItemQuality(Item item)
@@ -49,15 +41,12 @@ namespace csharp
 
         private static void UpdateItemQualitySellInNotExpired(Item item)
         {
+            if (item.Name == ProductNames.Sulfuras)
+                return;
+
             if (item.Name != ProductNames.AgedBrie && item.Name != ProductNames.BackstagePasses)
             {
-                if (item.Quality > MinQuality)
-                {
-                    if (item.Name != ProductNames.Sulfuras)
-                    {
-                        item.Quality -= QualityDecreaseStep;
-                    }
-                }
+                DecreaseQuality(item);
             }
             else
             {
@@ -76,46 +65,29 @@ namespace csharp
 
         private static void UpdateItemQualityForExpiredItems(Item item)
         {
-            if (item.SellIn < 0)
-            {
-                if (item.Name != ProductNames.AgedBrie)
-                {
-                    if (item.Name != ProductNames.BackstagePasses)
-                    {
-                        if (item.Quality > MinQuality)
-                        {
-                            if (item.Name != ProductNames.Sulfuras)
-                            {
-                                item.Quality -= QualityDecreaseStep;
-                            }
-                        }
-                    }
-                    else
-                    {
-                        item.Quality = MinQuality;
-                    }
-                }
-                else
-                {
-                    IncreaseQuality(item);
-                }
-            }
-        }
+            if (item.SellIn >= 0) return;
 
-        private static void IncreaseQuality(Item item)
-        {
-            if (item.Quality < MaxQuality)
-            {
-                item.Quality += QualityIncreaseStep;
-            }
+            if (item.Name == ProductNames.AgedBrie)
+                IncreaseQuality(item);
+            else if (item.Name == ProductNames.BackstagePasses)
+                item.Quality = MinQuality;
+            else if (item.Name != ProductNames.Sulfuras)
+                DecreaseQuality(item);
         }
 
         private static void ReduceSellInDays(Item item)
         {
-            if (item.Name != ProductNames.Sulfuras)
-            {
-                item.SellIn -= SellInDecreaseStep;
-            }
+            if (item.Name != ProductNames.Sulfuras) item.SellIn -= SellInDecreaseStep;
+        }
+
+        private static void IncreaseQuality(Item item)
+        {
+            if (item.Quality < MaxQuality) item.Quality += QualityIncreaseStep;
+        }
+
+        private static void DecreaseQuality(Item item)
+        {
+            if (item.Quality > MinQuality) item.Quality -= QualityDecreaseStep;
         }
     }
 }
