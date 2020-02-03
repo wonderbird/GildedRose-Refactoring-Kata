f29daa6d41883104df1c220a8276bd26e79f7cb2
Stefan Boos <kontakt@boos.systems>
2020-02-03 11:25:47 +0100

    add tests for requirements ... and one for the missing requirement "aged brie increases quality by 2 if it reached its sell date"

--- csharp/GildedRoseTest.cs
+++ csharp/GildedRoseTest.cs
@@ -1,5 +1,5 @@
-﻿using NUnit.Framework;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using NUnit.Framework;
 
 namespace csharp
 {
@@ -9,10 +9,217 @@ namespace csharp
         [Test]
         public void foo()
         {
-            IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
-            GildedRose app = new GildedRose(Items);
+            IList<Item> Items = new List<Item> {new Item {Name = "foo", SellIn = 0, Quality = 0}};
+            var app = new GildedRose(Items);
             app.UpdateQuality();
             Assert.AreEqual("fixme", Items[0].Name);
+        }
+
+        [Test]
+        public void UpdateQuality_SellInLarger0_ReducesQualityAndSellIn()
+        {
+            var arbitraryName = "Something";
+
+            var actualSellIn = 5;
+            var expectedSellIn = actualSellIn - 1;
+
+            var actualQuality = 10;
+            var expectedQuality = actualQuality - 1;
+
+            var item = new Item
+            {
+                Name = arbitraryName,
+                Quality = actualQuality,
+                SellIn = actualSellIn
+            };
+            var items = new List<Item> {item};
+
+            var rose = new GildedRose(items);
+            rose.UpdateQuality();
+
+            Assert.AreEqual(expectedSellIn, item.SellIn);
+            Assert.AreEqual(expectedQuality, item.Quality);
+        }
+
+        [Test]
+        public void UpdateQuality_SellIn0_ReducesQualityBy2AndSellIn()
+        {
+            var arbitraryName = "Something";
+
+            var actualSellIn = 0;
+            var expectedSellIn = actualSellIn - 1;
+
+            var actualQuality = 10;
+            var expectedQuality = actualQuality - 2;
+
+            var item = new Item
+            {
+                Name = arbitraryName,
+                Quality = actualQuality,
+                SellIn = actualSellIn
+            };
+            var items = new List<Item> { item };
+
+            var rose = new GildedRose(items);
+            rose.UpdateQuality();
+
+            Assert.AreEqual(expectedSellIn, item.SellIn);
+            Assert.AreEqual(expectedQuality, item.Quality);
+        }
+
+        [Test]
+        public void UpdateQuality_Quality0_DoesNotReduceQuality()
+        {
+            var arbitraryName = "Something";
+
+            var actualSellIn = 5;
+            var expectedSellIn = actualSellIn - 1;
+
+            var actualQuality = 0;
+            var expectedQuality = actualQuality;
+
+            var item = new Item
+            {
+                Name = arbitraryName,
+                Quality = actualQuality,
+                SellIn = actualSellIn
+            };
+            var items = new List<Item> { item };
+
+            var rose = new GildedRose(items);
+            rose.UpdateQuality();
+
+            Assert.AreEqual(expectedSellIn, item.SellIn);
+            Assert.AreEqual(expectedQuality, item.Quality);
+        }       
+        
+        [Test]
+        public void UpdateQuality_AgedBrie_IncreasesInQuality()
+        {
+            var agedBrie = "Aged Brie";
+
+            var actualSellIn = 5;
+            var expectedSellIn = actualSellIn - 1;
+
+            var actualQuality = 10;
+            var expectedQuality = actualQuality + 1;
+
+            var item = new Item
+            {
+                Name = agedBrie,
+                Quality = actualQuality,
+                SellIn = actualSellIn
+            };
+            var items = new List<Item> { item };
+
+            var rose = new GildedRose(items);
+            rose.UpdateQuality();
+
+            Assert.AreEqual(expectedSellIn, item.SellIn);
+            Assert.AreEqual(expectedQuality, item.Quality);
+        }
+
+        [Test]
+        public void UpdateQuality_AgedBrieQuality50_QualityStays50()
+        {
+            var agedBrie = "Aged Brie";
+
+            var actualSellIn = 5;
+            var expectedSellIn = actualSellIn - 1;
+
+            var actualQuality = 50;
+            var expectedQuality = actualQuality;
+
+            var item = new Item
+            {
+                Name = agedBrie,
+                Quality = actualQuality,
+                SellIn = actualSellIn
+            };
+            var items = new List<Item> { item };
+
+            var rose = new GildedRose(items);
+            rose.UpdateQuality();
+
+            Assert.AreEqual(expectedSellIn, item.SellIn);
+            Assert.AreEqual(expectedQuality, item.Quality);
+        }
+
+        [Test]
+        public void UpdateQuality_AgedBrieWithSellIn0_QualityIncreasesBy2()
+        {
+            var agedBrie = "Aged Brie";
+
+            var actualSellIn = 0;
+            var expectedSellIn = actualSellIn - 1;
+
+            var actualQuality = 10;
+            var expectedQuality = actualQuality + 2;
+
+            var item = new Item
+            {
+                Name = agedBrie,
+                Quality = actualQuality,
+                SellIn = actualSellIn
+            };
+            var items = new List<Item> { item };
+
+            var rose = new GildedRose(items);
+            rose.UpdateQuality();
+
+            Assert.AreEqual(expectedSellIn, item.SellIn);
+            Assert.AreEqual(expectedQuality, item.Quality);
+        }
+
+        [Test]
+        public void UpdateQuality_Sulfuras_DoesNotDecreaseQuality()
+        {
+            var sulfuras = "Sulfuras, Hand of Ragnaros";
+
+            var actualSellIn = 5;
+            var expectedSellIn = actualSellIn;
+
+            var actualQuality = 30;
+            var expectedQuality = actualQuality;
+
+            var item = new Item
+            {
+                Name = sulfuras,
+                Quality = actualQuality,
+                SellIn = actualSellIn
+            };
+            var items = new List<Item> { item };
+
+            var rose = new GildedRose(items);
+            rose.UpdateQuality();
+
+            Assert.AreEqual(expectedSellIn, item.SellIn);
+            Assert.AreEqual(expectedQuality, item.Quality);
+        }
+
+        [TestCase(11, 10, 20, 21)]
+        [TestCase(10, 9, 20, 22)]
+        [TestCase(6, 5, 20, 22)]
+        [TestCase(5, 4, 20, 23)]
+        [TestCase(1, 0, 20, 23)]
+        [TestCase(0, -1, 20, 0)]
+        public void UpdateQuality_BackstagePasses_QualityAsExpected(int actualSellIn, int expectedSellIn, int actualQuality, int expectedQuality)
+        {
+            var backstagePasses = "Backstage passes to a TAFKAL80ETC concert";
+
+            var item = new Item
+            {
+                Name = backstagePasses,
+                Quality = actualQuality,
+                SellIn = actualSellIn
+            };
+            var items = new List<Item> { item };
+
+            var rose = new GildedRose(items);
+            rose.UpdateQuality();
+
+            Assert.AreEqual(expectedSellIn, item.SellIn);
+            Assert.AreEqual(expectedQuality, item.Quality);
         }
     }
 }
