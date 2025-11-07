897a7bd10d0652a36001965878d392fea57c09d5
Stefan Boos + Cursor (Gemini 2.5 Pro) <galadriel@example.com>
2025-11-07 05:05:47 +0100

    @ F remove all unit tests

--- csharp.xUnit/GildedRoseTests/GildedRoseTest.cs
+++ csharp.xUnit/GildedRoseTests/GildedRoseTest.cs
@@ -7,11 +7,8 @@ namespace GildedRoseTests;
 public class GildedRoseTest
 {
     [Fact]
-    public void foo()
+    public void EmptyTest()
     {
-        IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
-        GildedRose app = new GildedRose(Items);
-        app.UpdateQuality();
-        Assert.Equal("fixme", Items[0].Name);
+        Assert.Fail("Not implemented");
     }
 }
